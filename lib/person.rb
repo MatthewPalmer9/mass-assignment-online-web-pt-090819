6eb4c61bdbf13fdef78450b243359a6aa8c84bd0
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-15 01:17:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complextion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(hash)
-    hash.each(){|key, value| self.send("#{key}",value)}
+  def initialize(attribute_hash)
+    attribute_hash.each(){|key, value| self.send("#{key}=", value)}
   end
 end
